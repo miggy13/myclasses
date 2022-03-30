3bfebece28e86c2dba903b164876c94437ec33ed
miggy13 <100565825+miggy13@users.noreply.github.com>
2022-03-30 18:13:33 +0800

    done my classes container

--- view.js
+++ view.js
@@ -1,10 +1,20 @@
-const list = document.querySelector(".list");
-const grid = document.querySelector(".grid");
-const ltg = document.querySelector(".ltg");
+function toggleView(e) {
+    console.log(e.target);
+    if (e.target.classList.contains('list-layout')) {
+        console.log('list layout');
+        document.querySelector('.toggle-btns').children[0].classList.add('active');
+        document.querySelector('.toggle-btns').children[1].classList.remove('active');
 
-list.addEventListener("click", () => {
-    ltg.classList.add("list")
-})
-grid.addEventListener("click", () => {
-    ltg.classList.remove("list");
-})
+        document.querySelector('.class-container').classList.add('list');
+        document.querySelector('.class-container').classList.remove('grid');
+
+    } else if (e.target.classList.contains('grid-layout')) {
+        console.log('grid layout');
+        document.querySelector('.toggle-btns').children[0].classList.remove('active');
+        document.querySelector('.toggle-btns').children[1].classList.add('active');
+
+        document.querySelector('.class-container').classList.remove('list');
+        document.querySelector('.class-container').classList.add('grid');
+
+    }
+}
